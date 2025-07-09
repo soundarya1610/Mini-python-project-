ef4dc8ab6d9a2b62bab6f5fabeb6b8e5ccf81cf3
soundarya1610 <soundaryask16@gmail.com>
2025-07-09 11:22:58 +0530

    Create student_id_card_generator.py

---
+++ student_id_card_generator.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+college = input("Enter your college: ")
+department = input("Enter your department: ")
+roll_number = int(input("Enter your roll number: "))
+
+print("\n--------STUDENT ID CARD--------", end="\n\n")
+print(f"Name     : {name}")
+print(f"College  : {college}")
+print(f"Dept     : {department}")
+print(f"Roll No. : {roll_number}")
+print("\n--------------------------------")
